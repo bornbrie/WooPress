9ce242ecb744987acc77e6aba3fb24e1606a27c4
Brianna Lee <brianna.lee@accretivetg.com>
2019-12-28 19:17:24 -0800

    Removed -d to show docker logs

--- init-woo.sh
+++ init-woo.sh
@@ -15,8 +15,4 @@ cd WooPress || exit 1
 echo "Done"
 echo "Initializing docker-compose network"
 docker-compose build --no-cache
-docker-compose up -d
-echo "Done"
-echo "Opening WooPress in browser..."
-open http://localhost/wp-admin
-echo "Done"
+docker-compose up
