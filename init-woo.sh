79a5ef3cb23a7b35038938b5f1f159e0d4064fa6
Brianna Lee <brianna.lee@accretivetg.com>
2019-12-28 19:09:17 -0800

    Added auto cleanup to script

--- init-woo.sh
+++ init-woo.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-
+echo "Cleaning up before install..."
+if [ -d "./WooPress" ]
+then
+  rm -rf WooPress
+fi
+yes | docker system prune -a
+docker container stop $(docker container ls -aq)
+echo "Done with cleanup"
 echo "Cloning WooPress to ${PWD}"
 git clone https://github.com/bornbrie/WooPress.git
 echo "Success"
