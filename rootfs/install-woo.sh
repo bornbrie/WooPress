ed2718090b3386a676a50c339a0237c2d6fe5afe
Brianna Lee <brianna.lee@accretivetg.com>
2019-12-30 00:37:56 -0800

    Added double brackets to if statements

--- rootfs/install-woo.sh
+++ rootfs/install-woo.sh
@@ -3,59 +3,59 @@
 cd /opt/bitnami/wordpress/wp-content/plugins || exit
 
 echo "Removing default plugins from WordPress installation..."
-if [ -d "./akismet"]
+if [[ -d "./akismet" ]]
 then
   rm -rf akismet
 fi
 
-if [ -d "./all-in-one-seo-pack" ]
+if [[ -d "./all-in-one-seo-pack" ]]
 then
   rm -rf all-in-one-seo-pack
 fi
 
-if [ -d "./all-in-one-wp-migration" ]
+if [[ -d "./all-in-one-wp-migration" ]]
 then
   rm -rf all-in-one-seo-pack
 fi
 
-if [ -d "./google-analytics-for-wordpress" ]
+if [[ -d "./google-analytics-for-wordpress" ]]
 then
   rm -rf all-in-one-seo-pack
 fi
 
-if [ -d "./jetpack" ]
+if [[ -d "./jetpack" ]]
 then
   rm -rf all-in-one-seo-pack
 fi
 
-if [ -d "./simple-tags" ]
+if [[ -d "./simple-tags" ]]
 then
   rm -rf all-in-one-seo-pack
 fi
 
-if [ -d "./w3-total-cache" ]
+if [[ -d "./w3-total-cache" ]]
 then
   rm -rf all-in-one-seo-pack
 fi
 
-if [ -d "./wp-cloud-mgmt-console" ]
+if [[ -d "./wp-cloud-mgmt-console" ]]
 then
   rm -rf all-in-one-seo-pack
 fi
 
-if [ -d "./wp-mail-smtp" ]
+if [[ -d "./wp-mail-smtp" ]]
 then
   rm -rf all-in-one-seo-pack
 fi
 
-if [ -f "./hello.php" ]
+if [[ -f "./hello.php" ]]
 then
   rm -rf all-in-one-seo-pack
 fi
 
-if [ -d "./WooOS-Storefront" ]
+if [[ -d "./WooStorefront" ]]
 then
-  rm -rf WooOS-Storefront
+  rm -rf WooStorefront
 fi
 
 echo "Done"
